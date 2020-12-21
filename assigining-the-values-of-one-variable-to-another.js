d07fd64adb480275ac44010779b4656ef0bea7e0
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-21 21:02:49 +0600

    assigining value to variable

---
+++ assigining-the-values-of-one-variable-to-another.js
@@ -0,0 +1,9 @@
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/assigning-the-value-of-one-variable-to-another
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
