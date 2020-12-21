29a5dfb61003ec0e9fb18cb69f79269349b4c04b
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-21 21:01:19 +0600

    storing values

---
+++ stroing-values-with-the-assignment-operator.js
@@ -0,0 +1,6 @@
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
