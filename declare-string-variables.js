f8448ad521fa291548a8fb9bffb628a986c3ed70
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-21 21:36:45 +0600

    declaring string variables

---
+++ declare-string-variables.js
@@ -0,0 +1,6 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-string-variables
+
+var myFirstName;
+var myLastName;
+myFirstName = "Sajedur"
+myLastName = "Rahman"
