8b0e8d37c8c457adc464b70bd673c3fe1d366e72
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-21 21:42:45 +0600

    escaping double quotes

---
+++ escaping-literal-quotes-in-strings.js
@@ -0,0 +1,3 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/escaping-literal-quotes-in-strings
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
