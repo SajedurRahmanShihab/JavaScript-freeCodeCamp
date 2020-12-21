9bb492ee40595343dd4f4cfd399e00b026d9517e
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-21 21:21:02 +0600

    decimal numbers

---
+++ create-decimal-numbers-with-javascript.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/create-decimal-numbers-with-javascript
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 14.5;
