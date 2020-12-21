19f679f0943add7b4e815beceba9ef2d5d8f1b73
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-21 21:16:28 +0600

    increment

---
+++ increment-a-number-with-javascript.js
@@ -0,0 +1,6 @@
+// freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/increment-a-number-with-javascript
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
