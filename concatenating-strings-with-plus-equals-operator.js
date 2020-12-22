29d68fb178616546a8903f80af25772fc2169181
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 11:43:36 +0600

    concatenating

---
+++ concatenating-strings-with-plus-equals-operator.js
@@ -0,0 +1,6 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/concatenating-strings-with-the-plus-equals-operator
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
