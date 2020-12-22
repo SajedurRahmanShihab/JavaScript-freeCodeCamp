2c545aae2094ea11518f73489f4e2ce68ee3be91
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 14:59:26 +0600

    word blank

---
+++ word-blank.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/word-blanks
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = myNoun + " " +myAdjective + " " +myVerb + " " +myAdverb; // Change this line
+// Only change code above this line
