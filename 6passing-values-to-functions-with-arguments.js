9fb5c87dbaaf71ac7cca6e9784c7d1e4fb9ee4cc
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 15:28:52 +0600

    passing arguement to a function

---
+++ 6passing-values-to-functions-with-arguments.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/passing-values-to-functions-with-arguments
+
+function functionWithArgs(param1, param2){
+    console.log(param1 + param2);  
+}
+
+functionWithArgs(2,4);
