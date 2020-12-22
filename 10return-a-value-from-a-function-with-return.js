07b212e13a97da1a363b687994257cee83e7bfdb
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 15:53:04 +0600

    return value

---
+++ 10return-a-value-from-a-function-with-return.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/return-a-value-from-a-function-with-return
+
+function timesFive(num){
+    return num * 5;
+}
+
+var answer = timesFive(5);
