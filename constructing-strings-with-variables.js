395a2b5011c548d1328a85f239c8b02762f1a494
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 11:43:56 +0600

    constructing strigs with variables

---
+++ constructing-strings-with-variables.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/constructing-strings-with-variables
+
+// Only change code below this line
+var myName = "Sajedur Rahman";
+var myStr = "My name is " + myName + " and I ma well!";
