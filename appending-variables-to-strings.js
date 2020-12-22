f16645cab8c2117f1811866de138cc94368fad91
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 11:46:10 +0600

    appending variables

---
+++ appending-variables-to-strings.js
@@ -0,0 +1,7 @@
+// freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/appending-variables-to-strings
+
+// Change code below this line
+
+var someAdjective = "fun!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
