a1f258c438ec8ef1011737dccd63bf286760d264
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 15:16:21 +0600

    unshift method

---
+++ 2Manipulate-arrays-with-unshift().js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-unshift
+
+// unshift use to add element at the beginning of an array
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.unshift(["Paul", 35]);
+
+// Only change code below this line
