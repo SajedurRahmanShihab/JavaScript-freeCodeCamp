ae75f3e12421068949f7d146530e1459dde2a85f
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 15:12:49 +0600

    shift() method

---
+++ 1Manipulate-arrays-with-shift.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-shift
+
+// shift() use to remove first item of array
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
