c6c7e8f341f6cde1f27d017f0a6d17262776cdb7
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 15:03:51 +0600

    access data in the array

---
+++ access-array-data-with-indexed.js
@@ -0,0 +1,6 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/access-array-data-with-indexes
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0];
+// Only change code below this line
