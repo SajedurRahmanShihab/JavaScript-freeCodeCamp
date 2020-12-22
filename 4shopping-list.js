9cc8ef1da0b7e2aba0d8182896b4a4b99d8aa0f3
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-22 15:25:08 +0600

    list in array

---
+++ 4shopping-list.js
@@ -0,0 +1,3 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/shopping-list
+
+var myList = [["Chocolate Bar", 15],["Potato Wedges", 30],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15]]
