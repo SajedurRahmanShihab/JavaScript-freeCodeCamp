abf9c1c7a6a5d389dfcfd0dc9b4a95b09de2e927
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-23 16:25:52 +0600

    accessing object properties

---
+++ 40Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-object-properties-with-dot-notation
+// Setup
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  
+  var hatValue = testObj.hat;      // Change this line
+  var shirtValue = testObj.shirt;    // Change this line
