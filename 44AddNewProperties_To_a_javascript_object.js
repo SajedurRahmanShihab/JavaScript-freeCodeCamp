911ee7f27d4ba85a1d20de581daa882794aa48a6
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-23 16:43:40 +0600

    adding new properties to a js object

---
+++ 44AddNewProperties_To_a_javascript_object.js
@@ -0,0 +1,12 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-new-properties-to-a-javascript-object
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  myDog.bark = "woof";
