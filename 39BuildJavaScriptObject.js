9d5b15cb26efd21362f63bc98994491122543e52
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-23 16:23:50 +0600

    JS object

---
+++ 39BuildJavaScriptObject.js
@@ -0,0 +1,11 @@
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/build-javascript-objects
+
+var myDog = {
+    // Only change code below this line
+    "name": "Evan",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["brad", "newboston"]    
+    
+    // Only change code above this line
+    };
