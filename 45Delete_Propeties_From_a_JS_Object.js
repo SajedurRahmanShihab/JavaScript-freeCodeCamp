d72ad6b4100d2a439e3efbe7aadcbbc02093f44b
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-23 16:49:40 +0600

    deleting object from js object

---
+++ 45Delete_Propeties_From_a_JS_Object.js
@@ -0,0 +1,13 @@
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/delete-properties-from-a-javascript-object
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
