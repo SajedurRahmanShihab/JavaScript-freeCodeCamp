d8f9a6901b85ef0505ef316f55145db0be16620d
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-24 17:12:47 +0600

    accessing nested object

---
+++ 49Accessing_Nested_Object.js
@@ -0,0 +1,15 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-nested-objects
+// Setup
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
