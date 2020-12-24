5fa4564976fa397d8e4cfe493ea0bd8b29585ecd
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-24 16:59:04 +0600

    manipulating complex object

---
+++ 48Manipulating_Complex_Objects.js
@@ -0,0 +1,22 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulating-complex-objects
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+
+    },
+    // Add a record here
+    {
+    "artist": "Shihab",
+    "title": "Sesh Bikeler Alo",
+    "release_year": 2020,
+    "formats": ["Metal", "Rock"]
+}
+  ];
