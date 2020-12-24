70127e6321e9fe49e27dca9c4127af84775dd1f5
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-24 11:23:14 +0600

    function and object

---
+++ 46Using_Object_For_Lookups.js
@@ -0,0 +1,22 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/using-objects-for-lookups
+
+
+// Setup
+function phoneticLookup(val) {
+    var result = "";
+  
+    // Only change code below this line
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    }
+    result = lookup[val];
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
