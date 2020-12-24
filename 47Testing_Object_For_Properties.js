5efb8888bb3c8c143b76ac165e1feeef22b897e9
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-24 16:49:44 +0600

    hasOwnProperty

---
+++ 47Testing_Object_For_Properties.js
@@ -0,0 +1,15 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/testing-objects-for-properties
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if(obj.hasOwnProperty(checkProp))
+    {
+        return obj[checkProp];
+    }
+    else
+    {
+        return "Not Found";
+    }
+    return "Change Me!";
+    // Only change code above this line
+  }
