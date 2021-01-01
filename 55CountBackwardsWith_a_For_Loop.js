dac8107e161526d1f3183e65dab255c6e3c58b6b
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-01-01 23:34:09 +0600

    count backward

---
+++ 55CountBackwardsWith_a_For_Loop.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/count-backwards-with-a-for-loop
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i = 9; i >= 1; i-=2){
+    myArray.push(i);
+}
