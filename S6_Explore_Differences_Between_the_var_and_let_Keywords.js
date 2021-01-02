e712f777ab5fe33101cc3ef4864b70b5e60817cb
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-01-03 04:41:36 +0600

    es6

---
+++ S6_Explore_Differences_Between_the_var_and_let_Keywords.js
@@ -0,0 +1,12 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/es6/explore-differences-between-the-var-and-let-keywords
+
+let catName;
+let quote;
+function catTalk() {
+
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
