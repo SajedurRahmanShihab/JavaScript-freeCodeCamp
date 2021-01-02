ddc7201f68ce164950114f5b720e70b36c2be041
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-01-03 04:41:26 +0600

    basic javascript completed

---
+++ 69UseRecursionToCreateARangeOfNumbers.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-recursion-to-create-a-range-of-numbers
+
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+      return [startNum];
+    } else {
+      var numbers = rangeOfNumbers(startNum, endNum - 1);
+      numbers.push(endNum);
+      return numbers;
+    }
+  }
